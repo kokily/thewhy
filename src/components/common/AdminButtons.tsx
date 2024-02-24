3cb2a2a855aea186a458cf09d538bdd9db77573b
kokily <kokily@naver.com>
2024-02-24 13:06:12 +0900

    Common Components(Etc...)

---
+++ src/components/common/AdminButtons.tsx
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { Modal } from './Modal';
+import { media } from '@/helper/client/style';
+
+interface Props {
+  modal: boolean;
+  onRemoveClick: () => void;
+  onConfirm: () => void;
+  onCancel: () => void;
+  onUpdate: () => void;
+}
+
+export function AdminButtons(props: Props) {
+  return (
+    <ButtonsContainer>
+      <ButtonsButton className="remove" onClick={props.onRemoveClick}>
+        삭 제
+      </ButtonsButton>
+
+      <ButtonsButton className="update" onClick={props.onUpdate}>
+        수 정
+      </ButtonsButton>
+
+      <Modal
+        visible={props.modal}
+        onCancel={props.onCancel}
+        onConfirm={props.onConfirm}
+      />
+    </ButtonsContainer>
+  );
+}
+
+// Styles
+const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1110px;
+  justify-content: center;
+  margin-bottom: 2rem;
+
+  ${media.large} {
+    max-width: 760px;
+  }
+
+  ${media.medium} {
+    max-width: 95%;
+  }
+`;
+
+const ButtonsButton = styled.button`
+  font-size: 0.9rem;
+  font-weight: 600;
+  padding: 0.5rem 1.5rem;
+  margin-right: 0.5rem;
+  border-radius: 15px;
+  cursor: pointer;
+  background: white;
+  box-shadow: 2px 2px 4px 0px gray;
+  transition: 0.12s;
+
+  &.remove {
+    color: red;
+    border: 2px solid red;
+
+    &:hover {
+      background: red;
+      color: white;
+    }
+
+    &:active {
+      background: red;
+      color: white;
+      transform: translateY(2px);
+      box-shadow: none;
+    }
+  }
+
+  &.update {
+    color: #00c2a8;
+    border: 2px solid #00c2a8;
+
+    &:hover {
+      background: #00c2a8;
+      color: white;
+    }
+
+    &:active {
+      background: #00c2a8;
+      color: white;
+      transform: translateY(2px);
+      box-shadow: none;
+    }
+  }
+`;
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,86 @@
+import type { ReactNode } from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  submit?: boolean;
+  back?: boolean;
+  red?: boolean;
+  onClick?: any;
+  children: ReactNode;
+}
+
+export function Button(props: Props) {
+  const htmlProps = props as any;
+
+  return (
+    <ButtonContainer
+      submit={props.submit}
+      back={props.back}
+      red={props.red}
+      {...htmlProps}
+      onClick={(e) => {
+        if (htmlProps.onClick) {
+          htmlProps.onClick(e);
+        }
+        (e.target as HTMLButtonElement).blur();
+      }}
+    >
+      {props.children}
+    </ButtonContainer>
+  );
+}
+
+// Styles
+const ButtonContainer = styled.button<{
+  submit?: boolean;
+  back?: boolean;
+  red?: boolean;
+}>`
+  font-size: 1rem;
+  font-weight: bold;
+  width: 90px;
+  padding: 0.5rem;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: 0.2s all;
+
+  &:active {
+    transform: translateY(1px);
+  }
+
+  ${(props) =>
+    props.submit &&
+    css`
+      border: 1px solid #06a4ff;
+      background: #06a4ff;
+      color: white;
+      &:hover {
+        background: white;
+        color: #06a4ff;
+      }
+    `}
+
+  ${(props) =>
+    props.back &&
+    css`
+      border: 1px solid #777;
+      background: #777;
+      color: white;
+      &:hover {
+        background: white;
+        color: #777;
+      }
+    `}
+    
+    ${(props) =>
+    props.red &&
+    css`
+      border: 1px solid #fa5252;
+      background: #fa5252;
+      color: white;
+      &:hover {
+        background: white;
+        color: #fa5252;
+      }
+    `}
+`;
